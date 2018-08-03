5972bb2d60ee673307ec42b84e075a59d07c8367
Erick Salas <stumpestro@ermiry.com>
2018-08-03 12:41:55 -0500

    SDL window working
    Test SDL window working fine. Opening and quitting as expected. It seems it is using a lot of CPU.

--- src/main.c
+++ src/main.c
@@ -13,8 +13,25 @@ typedef uint64_t u64;
 typedef int32_t i32;
 typedef int64_t i64;
 
+/*** SCREEN ***/
+
+void renderScreen (SDL_Renderer *renderer, SDL_Texture *screen) {
+
+    u32 *pixels = (u32 *) calloc (SCREEN_WIDTH * SCREEN_HEIGHT, sizeof (u32));
+
+    SDL_UpdateTexture (screen, NULL, pixels, SCREEN_WIDTH * sizeof (u32));
+    SDL_RenderClear (renderer);
+    SDL_RenderCopy (renderer, screen, NULL, NULL);
+    SDL_RenderPresent (renderer);
+
+}
+
+
+/*** THREAD ***/
+
 int main (void) {
 
+    // SDL SETUP
     SDL_Init (SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow ("Blackrock Dungeons",
          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
@@ -26,6 +43,27 @@ int main (void) {
 
     SDL_Texture *screen = SDL_CreateTexture (renderer, SDL_PIXELFORMAT_RGBA8888, 
         SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Main loop
+    bool done = false;
+    while (!done) {
+        SDL_Event event;
+        while (SDL_PollEvent (&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                done = true;
+                break;
+            }
+        }
+
+        renderScreen (renderer, screen);
+    }
+
+
+    // SDL CLEANUP
+    SDL_DestroyRenderer (renderer);
+    SDL_DestroyWindow (window);
+
+    SDL_Quit ();
 
     return 0;
 
